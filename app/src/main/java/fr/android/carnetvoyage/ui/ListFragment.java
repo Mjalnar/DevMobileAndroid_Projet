6cb55b151dad7515b70d1f61722c14dd51aad199
VictorBonnin <bonnin.victor197@gmail.com>
2026-06-02 18:05:46 +0200

    PartieB : ajout des modifications et nouveaux fichiers

--- app/src/main/java/fr/android/carnetvoyage/ui/ListFragment.java
+++ app/src/main/java/fr/android/carnetvoyage/ui/ListFragment.java
@@ -1,21 +1,58 @@
 package fr.android.carnetvoyage.ui;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import fr.android.carnetvoyage.R;
 
-public class MapFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.android.carnetvoyage.R;
+import fr.android.carnetvoyage.data.LocalRepository;
+import fr.android.carnetvoyage.model.Entry;
+
+public class ListFragment extends Fragment {
+
+    private TextView textCount;
+    private ListView listView;
+    private LocalRepository repository;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        textCount = view.findViewById(R.id.text_count);
+        listView = view.findViewById(R.id.list_view_simple);
+        repository = new LocalRepository(requireContext());
+
+        refreshList();
+
+        return view;
+    }
+
+    private void refreshList() {
+        List<Entry> entries = repository.getAll();
+        textCount.setText("Nombre d'entrées : " + entries.size());
+
+        List<String> displayList = new ArrayList<>();
+        for (Entry e : entries) {
+            displayList.add(e.getTitle() + "\n" + e.getAddress());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                displayList
+        );
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/fr/android/carnetvoyage/ui/MapFragment.java
+++ app/src/main/java/fr/android/carnetvoyage/ui/MapFragment.java
@@ -9,12 +9,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import fr.android.carnetvoyage.R;
 
-public class ListFragment extends Fragment {
+public class MapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
